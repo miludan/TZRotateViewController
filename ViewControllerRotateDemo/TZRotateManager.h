2c6a9a2b0d36ddcd089d870a4cf5edf5dfd40a9a
章舜 <zhangshun@qiyi.com>
2018-06-03 21:03:22 +0800

    remove licensing info.

--- ViewControllerRotateDemo/TZRotateManager.h
+++ ViewControllerRotateDemo/TZRotateManager.h
@@ -3,7 +3,6 @@
 //  ViewControllerRotateDemo
 //
 //  Created by Tomas_Zhang on 2018/4/17.
-//  Copyright © 2018年 Neulion. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- ViewControllerRotateDemo/ViewController.m
+++ ViewControllerRotateDemo/ViewController.m
@@ -3,7 +3,6 @@
 //  ViewControllerRotateDemo
 //
 //  Created by Tomas_Zhang on 2018/3/30.
-//  Copyright © 2018年 Neulion. All rights reserved.
 //
 
 #import "ViewController.h"
